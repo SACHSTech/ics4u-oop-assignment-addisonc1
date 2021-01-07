9053eb96b70b027d33e3f74a2d509632f91a1797
addisonc1 <addison.chan21@ycdsbk12.ca>
2021-01-07 04:54:27 +0000

    added new examples

--- src/oop/Main.java
+++ src/oop/Main.java
@@ -14,9 +14,17 @@ public class Main{
     System.out.println("Color: white,black,green");
     System.out.println("Stock:10");
     System.out.println("");
+    System.out.println("Jeans");
+    System.out.println("Color: Blue");
+    System.out.println("Stock:10");
+    System.out.println("");
     System.out.println("Shirts:");
     System.out.println("Tshirt");
     System.out.println("Color:black,grey,white");
+    System.out.println("Stock:10");
+    System.out.println("");
+    System.out.println("Longsleeve Shirt");
+    System.out.println("Color:white");
     System.out.println("Stock:10");
     System.out.println("");
     System.out.println("Sweaters:");
@@ -24,18 +32,25 @@ public class Main{
     System.out.println("Color:black,white,blue");
     System.out.println("Stock:10");
     System.out.println("");
-
+    System.out.println("Christmas Sweater");
+    System.out.println("Color:Christmas");
+    System.out.println("Stock:10");
+    System.out.println("");
+    
     //all items
     pants whitejoggers = new pants("whitejogger","joggers" ,"white", 10);
     pants blackjoggers = new pants("blackjogger","joggers" ,"black", 10);
     pants greenjoggers = new pants("greenjogger","joggers" ,"green", 10);
+    pants bluejeans = new pants("bluejeans","jeans" ,"blue", 10);
     shirts blackshirts = new shirts("blackshirt","tshirt" ,"black", 10);
     shirts greyshirts = new shirts("greyshirt","tshirt" ,"grey", 10);
     shirts whiteshirts = new shirts("whiteshirt","tshirt" ,"white", 10);
+    shirts whitelongsleeve = new shirts("whitelongsleeveshirt","longsleeveshirt" ,"white", 10);
     sweaters blacksweaters = new sweaters("blacksweater","hoodie","black",10);
     sweaters whitesweaters = new sweaters("whitesweater","hoodie","white",10);
     sweaters bluesweaters = new sweaters("bluesweater","hoodie","blue",10);
-    
+    sweaters christmassweater = new sweaters("christmassweater","sweater","christmas",10);
+
     //the cart items
     items addison = new items("addison@gmail.com");
   
